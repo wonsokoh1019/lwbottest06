bd8227712319dc0fd0c7054a0c2d07c93938e429
wonsokoh1019 <56017035+wonsokoh1019@users.noreply.github.com>
2019-11-28 11:47:32 +0900

    Update config.py

--- conf/config.py
+++ conf/config.py
@@ -6,22 +6,22 @@ import os
 TZone = "Asia/Tokyo"
 
 # account
-ADMIN_ACCOUNT = "admin@lwbottest04"
-DOMAIN_ID = 153499
+ADMIN_ACCOUNT = "admin@lwbottest06"
+DOMAIN_ID = 153691
 
 # api
-API_ID = "kr1hLBbxHJiDs"
-SERVER_CONSUMER_KEY = "cqRT31EkE5TFcSZCkIzt"
+API_ID = "kr1JhIcRxJWyd"
+SERVER_CONSUMER_KEY = "c7omcU5lWDeQcnxSNEJ8"
 
-SERVER_ID = "c2d6d1eb148b4144a9f75abe49faece0"
-SECRET_KEY_NAME = "private_20191127124751.key"
+SERVER_ID = "5cf58b0c00594b6196153ca15d0f6516"
+SECRET_KEY_NAME = "private_20191128114106.key"
 
 # LOCAL ADDRESS
-LOCAL_ADDRESS = "https://pm191127samplebot.herokuapp.com/"
+LOCAL_ADDRESS = "https://lwbottest06.herokuapp.com/"
 
 # DB config
-DB_HOST = "ec2-54-235-248-185.compute-1.amazonaws.com"
-DB_NAME = "d948ducpbs8to4"
-DB_USER = "irxbslbpfagcyw"
+DB_HOST = "ec2-23-21-248-1.compute-1.amazonaws.com"
+DB_NAME = "d3k3fqgo475ret"
+DB_USER = "vxewlzhwumxkdq"
 DB_PORT = "5432"
-DB_PASSWORD = "4484cc1abc190bd1bc9c46da75ff2f67df260963ee3e87f0eb7fd2eecd3942d3"
+DB_PASSWORD = "79bee26ff0767c54f665bad94d188f8219eacd82a91c359ce96ad65282592de6"
